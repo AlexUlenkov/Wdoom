02127d68c9a5188e871e8ca40b330cd0340c2d68
Alexey Ulenkov <alexey.ulenkov@gmail.com>
2016-07-24 00:18:37 +0300

    Initial Commit

---
+++ WarningDoom/NSObject_Extension.h
@@ -0,0 +1,16 @@
+//
+//  NSObject_Extension.h
+//  WarningDoom
+//
+//  Created by ALEXEY ULENKOV on 24.07.16.
+//  Copyright © 2016 Alexey Ulenkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Xcode_Plugin_Template_Extension)
+
++ (void)pluginDidLoad:(NSBundle *)plugin;
+
+
+@end
---
+++ WarningDoom/NSObject_Extension.m
@@ -0,0 +1,25 @@
+//
+//  NSObject_Extension.m
+//  WarningDoom
+//
+//  Created by ALEXEY ULENKOV on 24.07.16.
+//  Copyright © 2016 Alexey Ulenkov. All rights reserved.
+//
+
+
+#import "NSObject_Extension.h"
+#import "WarningDoom.h"
+
+@implementation NSObject (Xcode_Plugin_Template_Extension)
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static dispatch_once_t onceToken;
+    NSString *currentApplicationName = [[NSBundle mainBundle] infoDictionary][@"CFBundleName"];
+    if ([currentApplicationName isEqual:@"Xcode"]) {
+        dispatch_once(&onceToken, ^{
+            sharedPlugin = [[WarningDoom alloc] initWithBundle:plugin];
+        });
+    }
+}
+@end
